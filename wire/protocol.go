7f5e751593f3fd6180fab799c869f1181f9457cb
paoner <paoner@gmail.com>
2022-06-09 17:01:32 +0800

    change testnet num

--- wire/protocol.go
+++ wire/protocol.go
@@ -122,7 +122,7 @@ const (
 	RegTest CurrencyNet = RegNet
 
 	// TestNet3 represents the 3rd test network.
-	TestNet3 CurrencyNet = 0xb9240ad9
+	TestNet3 CurrencyNet = 0xb9240ada
 
 	// SimNet represents the simulation test network.
 	SimNet CurrencyNet = 0xa7974632
